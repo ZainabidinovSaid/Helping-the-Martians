23208ecc459431c5f5a86968b6003f02204e2f82
unknown <saidislam1233@gmail.com>
2023-11-22 17:30:06 +0600

    third commit

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ def initialize_boxes():
         {"kilometers": random.randint(1, 10), "weight": random.randint(100, 300)},
         {"kilometers": random.randint(1, 10), "weight": random.randint(100, 300)}
     ]
+def move_boxes():
+    for box in boxes:
+        box["kilometer"] = random.randint(1, 10)
+
