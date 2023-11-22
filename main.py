7f962d51e414326a6d67a747c36470871b5de493
unknown <saidislam1233@gmail.com>
2023-11-22 17:27:22 +0600

    second commit

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 import random
+def initialize_boxes():
+    return [
+        {"kilometers": random.randint(1, 10), "weight": random.randint(100, 300)},
+        {"kilometers": random.randint(1, 10), "weight": random.randint(100, 300)},
+        {"kilometers": random.randint(1, 10), "weight": random.randint(100, 300)}
+    ]
