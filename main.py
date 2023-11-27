45e6d96556852c192c847ea8251f7a5bd96bdb5b
unknown <saidislam1233@gmail.com>
2023-11-27 18:46:34 +0600

    third commit

--- main.py
+++ main.py
@@ -9,3 +9,6 @@ def move_boxes():
     for box in boxes:
         box["kilometer"] = random.randint(1, 10)
 
+def check_total_weight():
+    total_weight = sum(box["weight"] for box in found_boxes)
+    return total_weight == 713
